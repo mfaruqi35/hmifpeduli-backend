dfad95f870336bc80b0369d5514d6737ef4442f4
mhdfaruqi23 <mfaruqi23@mhs.usk.ac.id>
2025-05-21 20:34:13 +0700

    deploy

--- routes/donationsRoute.js
+++ routes/donationsRoute.js
@@ -1,17 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
-import { createDonation, getDonationsByCampaign, verifyDonationStatus, getAllDonations } from "../controllers/donationController.js";
-import { optionalAuth } from "../middleware/middleware.js";
-
-const router = express.Router();
-
-router.post("/", optionalAuth, createDonation);
-router.get("/", getAllDonations); // di donationsRoute.js
-router.get("/donations/:campaignId", getDonationsByCampaign);
-router.patch("/verify/:donationId", verifyDonationStatus);
-
-export default router;
-=======
 import { authUser } from "../middleware/middleware.js";
 import {
   createDonation,
@@ -26,4 +13,3 @@ donationRouter.post("/guest", uploadImages, createDonation);
 donationRouter.get("/recent", getRecentDonation);
 
 export default donationRouter;
->>>>>>> 1def7b59942800ba3e9e3a8c4695b6d43d43826c
